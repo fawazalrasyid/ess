512fbc3cfd173b27b831ed7d2848b3b0e37e7abb
fawazalrasyid <alrasyid.fawaz16@gmail.com>
2024-05-09 13:53:12 +0700

    feat: login & register

---
+++ lib/app/modules/main/views/main_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_icons/flutter_svg_icons.dart';
+
+import 'package:get/get.dart';
+import 'package:iconly/iconly.dart';
+
+import '../../../core/values/app_colors.dart';
+import '../../home/views/home_view.dart';
+import '../controllers/main_controller.dart';
+
+class MainView extends GetView<MainController> {
+  MainView({super.key});
+
+  @override
+  final MainController controller = Get.put(MainController());
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<MainController>(
+      builder: (controller) {
+        return Scaffold(
+          body: IndexedStack(
+            index: controller.tabIndex.value,
+            children: const [
+              HomeView(),
+              HomeView(),
+              HomeView(),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: AppColors.white,
+            currentIndex: controller.tabIndex.value,
+            onTap: controller.changeTabIndex,
+            selectedItemColor: AppColors.selectedNavbarIcon,
+            unselectedItemColor: AppColors.navbarIcon,
+            type: BottomNavigationBarType.fixed,
+            iconSize: 24,
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.home),
+                label: 'Beranda',
+              ),
+              BottomNavigationBarItem(
+                icon: SvgIcon(
+                  icon: SvgIconData('assets/icons/fi-sr-map.svg'),
+                ),
+                label: 'Laporan',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(IconlyBold.setting),
+                label: 'Profil',
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,24 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const SPLASH = _Paths.SPLASH;
+  static const ONBOARDING = _Paths.ONBOARDING;
+  static const LOGIN = _Paths.LOGIN;
+  static const REGISTER = _Paths.REGISTER;
+  static const PRIVACY_POLICY = _Paths.PRIVACY_POLICY;
+  static const MAIN = _Paths.MAIN;
+  static const HOME = _Paths.HOME;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const SPLASH = '/splash';
+  static const ONBOARDING = '/onboarding';
+  static const LOGIN = '/login';
+  static const REGISTER = '/register';
+  static const PRIVACY_POLICY = '/privacy-policy';
+  static const MAIN = '/main';
+  static const HOME = '/home';
+}
